02cb3905498d6c7ec84e972650816e79f28825c5
Erick Jimenez <cmc.206.2018@gmail.com>
2026-06-09 14:59:23 -0600

    Panel carrito funcional

--- src/intento/PanelCarrito.java
+++ src/intento/PanelCarrito.java
@@ -9,14 +9,16 @@ package intento;
  * @author ErickJimz
  */
 public class PanelCarrito extends javax.swing.JPanel {
-
+    private double  precioUnitario =45.0;
     /**
      * Creates new form PanelCarrito
      */
     public PanelCarrito() {
         initComponents();
+        actualizarInterfaz(Integer.parseInt(lblCantidad.getText().trim()));
     }
-
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,11 +31,11 @@ public class PanelCarrito extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        lblCantidad = new javax.swing.JLabel();
+        btnSum = new javax.swing.JButton();
+        btnRes = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
+        lblPrecioTotal = new javax.swing.JLabel();
 
         jLabel1.setText("jLabel1");
 
@@ -41,84 +43,147 @@ public class PanelCarrito extends javax.swing.JPanel {
 
         jLabel3.setText("$10.00 c/u");
 
-        jLabel4.setText("2");
+        lblCantidad.setText("0");
 
-        jButton1.setText("+");
+        btnSum.setText("+");
+        btnSum.addActionListener(this::btnSumActionPerformed);
 
-        jButton2.setText("-");
-        jButton2.addActionListener(this::jButton2ActionPerformed);
+        btnRes.setText("-");
+        btnRes.addActionListener(this::btnResActionPerformed);
 
         jButton3.setForeground(new java.awt.Color(255, 51, 51));
         jButton3.setText("Eliminar ");
         jButton3.addActionListener(this::jButton3ActionPerformed);
 
-        jLabel5.setText("Precio");
+        lblPrecioTotal.setText("Precio");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblPrecioTotal)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(jLabel5))
-                            .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addComponent(jLabel3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
-                                .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel4)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)))
-                        .addGap(22, 22, 22)
-                        .addComponent(jButton3))))
+                                .addGap(41, 41, 41)))
+                        .addGap(29, 29, 29)
+                        .addComponent(btnSum)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblCantidad)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnRes)))
+                .addGap(22, 22, 22)
+                .addComponent(jButton3))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(84, 84, 84)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(84, 84, 84)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
-                            .addComponent(jLabel4)
-                            .addComponent(jButton1)
-                            .addComponent(jButton2)
+                            .addComponent(lblCantidad)
+                            .addComponent(btnSum)
+                            .addComponent(btnRes)
                             .addComponent(jButton3))
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel5))
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(137, Short.MAX_VALUE))
+                        .addComponent(lblPrecioTotal))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(62, 62, 62)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(41, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
+    
+    private void btnResActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResActionPerformed
+        try {
+        // 1. Obtenemos el texto actual del JLabel y lo convertimos a número
+        int cantidadActual = Integer.parseInt(lblCantidad.getText().trim());
+        
+        // 2. Validamos: Solo restamos si es mayor que 0 (así nunca será negativo)
+        if (cantidadActual > 0) {
+            cantidadActual--; // Restamos 1
+            
+            // 3. Actualizamos el JLabel con el nuevo valor
+            lblCantidad.setText(String.valueOf(cantidadActual));
+            actualizarInterfaz(cantidadActual);
+            // Aquí puedes actualizar también el subtotal si tu objeto 'carrito' lo requiere
+        }
+    } catch (NumberFormatException e) {
+        // Por si el JLabel llega a tener un texto no numérico por error
+        lblCantidad.setText("0");
+    }
+    }//GEN-LAST:event_btnResActionPerformed
+    
+    private void actualizarInterfaz(int cantidad) {
+        // Multiplicamos la cantidad actual por nuestro precio fijo por unidad
+        double nuevoPrecioTotal = cantidad * precioUnitario;
+        
+        // Modificamos los textos visuales en pantalla
+        lblCantidad.setText(String.valueOf(cantidad));
+        lblPrecioTotal.setText(String.format("$%.2f", nuevoPrecioTotal));
+    }
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        // 1. Preguntar al usuario si realmente desea eliminar el producto (Buena práctica)
+    int respuesta = javax.swing.JOptionPane.showConfirmDialog(
+            this, 
+            "¿Estás seguro de que deseas quitar este producto del carrito?", 
+            "Quitar producto", 
+            javax.swing.JOptionPane.YES_NO_OPTION
+    );
+    
+    // 2. Si el usuario responde que SÍ
+    if (respuesta == javax.swing.JOptionPane.YES_OPTION) {
+        
+        // Obtenemos el contenedor padre donde está metida toda esta línea/tarjeta
+        java.awt.Container padre = this.getParent();
+        
+        if (padre != null) {
+            // Le decimos al padre que elimine de sus componentes a este JComponent (this)
+            padre.remove(this);
+            
+            // CRÍTICO: Obligamos al contenedor a recalcular los espacios y rediseñarse
+            padre.revalidate();
+            padre.repaint();
+        }
+    }
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void btnSumActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSumActionPerformed
+        try {
+        // 1. Obtenemos el texto actual del JLabel
+        int cantidadActual = Integer.parseInt(lblCantidad.getText().trim());
+        
+        // 2. Sumamos 1 sin restricciones (o le pones un tope si hay límite de inventario)
+        cantidadActual++;
+        
+        lblCantidad.setText(String.valueOf(cantidadActual));
+        actualizarInterfaz(cantidadActual);
+        // Aquí también podrías recalcular el precio total de este producto
+    } catch (NumberFormatException e) {
+        lblCantidad.setText("1");
+    }
+    }//GEN-LAST:event_btnSumActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnRes;
+    private javax.swing.JButton btnSum;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lblCantidad;
+    private javax.swing.JLabel lblPrecioTotal;
     // End of variables declaration//GEN-END:variables
 }
