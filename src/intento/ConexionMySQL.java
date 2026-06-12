ce5710759b8adf8d574354c46c12cbc0b80194c0
RAGM80 <rgacer80@gmail.com>
2026-06-12 11:01:54 -0600

    ventas

--- src/intento/ConexionMySQL.java
+++ src/intento/ConexionMySQL.java
@@ -26,4 +26,8 @@ public class ConexionMySQL {
         }                   
         return enlace;
     }
+
+    Connection conectar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
