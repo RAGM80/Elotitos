80458dde9748d43817e1e6faa9db347cd2d4fcbf
Alejandro Perez <pepetruenoelchico@gmail.com>
2026-06-13 03:00:00 -0600

    Guardando mi versionbeta estable antes del pull

---
+++ src/versionbeta/ConexionMySQL.java
@@ -0,0 +1,33 @@
+package versionbeta; // Tu paquete actual
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Limon
+ */
+public class ConexionMySQL {
+    private final String db = "eloteria_db"; 
+    private final String url = "jdbc:mysql://localhost:3306/" + db;
+    private final String usuario = "root";
+    private final String pass = ""; 
+    
+    public ConexionMySQL() {
+    }
+    
+    public Connection Conectar() {
+        Connection enlace = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            enlace = DriverManager.getConnection(this.url, this.usuario, this.pass);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar la base de datos: " + e.getMessage(), "Error de Conexión", JOptionPane.ERROR_MESSAGE);
+        }                   
+        return enlace;
+    }
+
+    Connection conectar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
